a13432a6a40e1a3a752c7f1f0b5639a06161f0a8
Michxz <michxz@student.ubc.cs.ca>
2023-07-31 21:34:57 -0700

    - Create new java class - Create get functions for every variable

--- src/model/Branch.java
+++ src/model/Branch.java
@@ -1,5 +1,31 @@
 package model;
 
 public class Branch {
-    
+    private final int locId;
+    private final String streetAddress;
+    private final String city;
+    private final String province;
+
+    public Branch(int locId, String streetAddress, String city, String province) {
+        this.locId = locId;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.province = province;
+    }
+
+    public int getLocId(){
+        return locId;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
 }

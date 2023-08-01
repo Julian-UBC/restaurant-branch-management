cd97405f154b010ca5c64aa482c7fbf125d65e0a
Michxz <michxz@student.ubc.cs.ca>
2023-07-31 21:44:06 -0700

    create and populate table

---
+++ src/sql/scripts/databaseSetup.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Branches (
+    locID   INTEGER PRIMARY KEY,
+    streetAddress VARCHAR(50),
+    city    VARCHAR(50),
+    province    VARCHAR(50)
+)
+
+INSERT INTO Branches VALUES(111, "792 Montreal Road", "Ottawa", "Ontario")
+INSERT INTO Branches VALUES(112, "1257 Beaver Creek", "Thornhill", "Ontario")
+INSERT INTO Branches VALUES(113, "792 Montreal Road", "Vancouver", "British Columbia")
+INSERT INTO Branches VALUES(114, "1221 Robson Street", "Vancouver", "British Columbia")
+INSERT INTO Branches VALUES(115, "4795 Robson Street", "Vancouver", "British Columbia")

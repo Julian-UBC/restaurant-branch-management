aa797719ffd1726bf1b63d0875eb8c3d495afdf9
UBC Student <dshiang@students.cs.ubc.ca>
2023-08-02 16:14:33 -0700

    Update Reservation.java

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -1,5 +1,59 @@
 package model;
 
-public class Reservation {
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+class Reservations {
+    private final int rID;
+    private final int cID;
+    private final int locID;
+    private final int wID;
+    private final LocalDate rDate;
+    private final LocalTime rTime;
+    private final int numOfPeople;
+    private final char reservationName;
+
+    public Reservations(int rID, int cID, int locID, int wID, LocalDate rDate, LocalTime rTime, int numOfPeople,
+            char reservationName) {
+        this.rID = rID;
+        this.cID = cID;
+        this.locID = locID;
+        this.wID = wID;
+        this.rDate = rDate;
+        this.rTime = rTime;
+        this.numOfPeople = numOfPeople;
+        this.reservationName = reservationName;
+    }
+
+    public int getRID() {
+        return rID;
+    }
+
+    public int getCID() {
+        return cID;
+    }
+
+    public int getLocID() {
+        return locID;
+    }
+
+    public int getWID() {
+        return wID;
+    }
+
+    public LocalDate getRDate() {
+        return rDate;
+    }
+
+    public LocalTime getRTime() {
+        return rTime;
+    }
+
+    public int getNumOfPeople() {
+        return numOfPeople;
+    }
+
+    public char getReservationName() {
+        return reservationName;
+    }
 }

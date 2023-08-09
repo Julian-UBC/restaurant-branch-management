4cf0f49f04bdafe89e18bfb38af32b8fce3ba3b0
Julian <julian.widjaja02@gmail.com>
2023-08-09 00:25:31 -0700

    Fix merge conflict

--- src/database/DatabaseConnectionHandler.java
+++ src/database/DatabaseConnectionHandler.java
@@ -8,6 +8,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseConnectionHandler {
